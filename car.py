70fd2042574de392aff40d6c3a6d4bd4d80744b3
abz391 <33454221+abz391@users.noreply.github.com>
2017-11-07 15:08:32 +0100

    Update car.py

---
+++ car.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(21, GPIO.OUT)	#Ausgang Motoren rechts
+GPIO.setup(18, GPIO.OUT)	#Ausgang Motoren links
+
+
+d = GPIO.PWM(21, 2) 		#Kanal 18, 2Hz, green
+d.start(50)			#Tastverhältnis 50%
+
+s = GPIO.PWM(18, 2)		 #Kanal 20, 2Hz, yellow
+s.start(50) 			#Tastverhältnis 50%
+
+
+input('Press return to stop:')
+d.stop()
+s.stop()
+
+GPIO.cleanup()
